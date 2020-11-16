b38afbb94d3fc6f952f97d8b6e397ed75821fbac
dro-1 <lorddro1532@gmail.com>
2020-11-16 23:31:15 +0100

    Added Titles and implemented Contact Form

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
 let sections = document.querySelectorAll(".projects .project").length;
+let nameInput = document.querySelector("input.name");
+let emailInput = document.querySelector("input.email");
+let message = document.querySelector("textarea");
+let submitBtn = document.querySelector("button.submit-btn");
 const projectArr = gsap.utils.toArray(".projects .project");
 
 gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollToPlugin);
+
+document.querySelector(".project-btn").addEventListener("click", () => {
+  gsap.to(window, { duration: 1, scrollTo: { y: ".projects" } });
+});
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  var templateParams = {
+    name: nameInput.value,
+    email: emailInput.value,
+    message: message.value,
+  };
+
+  emailjs.send("contact_service", "contact_template", templateParams).then(
+    function (response) {
+      console.log("SUCCESS!", response.status, response.text);
+    },
+    function (error) {
+      console.log("FAILED...", error);
+    }
+  );
+});
 
 projectArr.map((project, index) => {
   if (index === projectArr.length - 1) {
@@ -15,7 +42,7 @@ projectArr.map((project, index) => {
     start: "top top",
     snap: {
       snapTo: 1,
-      duration: { min: 0.1, max: 0.3 },
+      duration: 1,
       delay: 0.05,
     },
     pin: true,
